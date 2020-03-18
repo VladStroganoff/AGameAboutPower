c0833577376f2f84753fc080ef4e52cfc5fcb702
Felix <felix.ronnholm@gmail.com>
2020-03-19 05:43:24 +0800

    Medling with save load for the tile word

--- AGameAboutPowerClient/Assets/TycoonTerrain/Scripts/Tools/Save.cs
+++ AGameAboutPowerClient/Assets/TycoonTerrain/Scripts/Tools/Save.cs
@@ -11,20 +11,20 @@ namespace TycoonTerrain.Core
 {
     public class Save : MonoBehaviour
     {
-        public TycoonTileMap map;
-
-        NetWorld save;
+        public TycoonTileMap TycoonMap;
         public TerrainGrid MapGrid;
         public TerrainTypeTable MapTypes;
         public string url;
 
+        private NetWorld save;
+        private NativeArray<byte> heightData;
         public Dictionary<int2, LandTile> tiles = new Dictionary<int2, LandTile>();
         public Dictionary<int2, ushort> tileTypes = new Dictionary<int2, ushort>();
 
         public void SaveMap()
         {
-            MapGrid = map.GetGrid();
-            MapTypes = map.TypeTable;
+            MapGrid = TycoonMap.GetGrid();
+            MapTypes = TycoonMap.TypeTable;
 
             save = new NetWorld();
             int2 pos = new int2(0, 0);
@@ -36,10 +36,6 @@ namespace TycoonTerrain.Core
                     pos = new int2(i, j);
                     TileHandle tile = MapGrid.GetTile(pos);
                     LandTile data = tile.GetData();
-
-
-                    if (tiles.ContainsKey(pos))
-                        Debug.Log("kuken?");
 
 
                     tiles.Add(pos, data);
@@ -76,24 +72,35 @@ namespace TycoonTerrain.Core
             tileTypes = new Dictionary<int2, ushort>();
         }
 
+        public void LoadMap()
+        {
+
+            if (!File.Exists(url))
+            {
+                FDebug.Log.Message("could not find file at: " + url);
+                return;
+            }
+
+            string text = File.ReadAllText(url);
+
+            NetWorld worldSave = JsonConvert.DeserializeObject<NetWorld>(text);
+            int size = worldSave.LandTiles.GetLength(0) * worldSave.LandTiles.GetLength(1);
+
+            for (int x = 0; x < worldSave.LandTiles.GetLength(0); x++)
+            {
+                for (int z = 0; z < worldSave.LandTiles.GetLength(1); z++)
+                {
+                    //heightData[x + size * z] = worldSave.LandTiles[x,z];
+                }
+            }
+
+        }
     }
 
-    [System.Serializable]
-    public class NetWorld
-    {
-        public NetLandTile[,] LandTiles;
-    }
 
-    public class NetLandTile
-    {
-        [SerializeField] public byte cornerNE;
-        [SerializeField] public byte cornerSE;
-        [SerializeField] public byte cornerSW;
-        [SerializeField] public byte cornerNW;
-        [SerializeField] public ushort waterHeight;
 
-        [SerializeField] public ushort tileType;
-    }
+
+   
 
 }
 
