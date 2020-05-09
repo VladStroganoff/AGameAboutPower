0a8705a0431082e28ecaf1a7c537e3b3fc96c3e2
Felix <felix.ronnholm@gmail.com>
2020-05-09 11:03:05 +0800

    adding texture array support for terrain shader

---
+++ GAP_Game/Assets/Scripts/Tools/TexturePacker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+
+
+public class TexturePacker : MonoBehaviour
+{
+    public Texture2D[] Textures;
+    public string SaveLocation = "Assets/TextureArray.tarr";
+
+    public void CreateTextureArray()
+    {
+        Texture2DArray array = new Texture2DArray(Textures[0].width, Textures[0].height, Textures.Length, Textures[0].format, false);
+
+
+        for (int i = 0; i < Textures.Length; i++)
+            array.SetPixels(Textures[i].GetPixels(), i);
+
+        array.Apply();
+        AssetDatabase.CreateAsset(array, SaveLocation);
+    }
+}
+
+
